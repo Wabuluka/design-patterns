225e283213a82fbeb0e0e1233758c074d0dd8b63
Wabuluka <dwabuluka@gmail.com>
2020-09-29 11:58:02 +0300

    eslint enforcement

--- cart.js
+++ cart.js
@@ -1,20 +1,13 @@
-const product = require('./product/product');
+// const product = require('./product/product');
+class Cart {
+  constructor() { this.carts = []; }
 
-class Cart{
-    constructor(){
-        this.carts = [];
-    }
+  addProduct(pdt) { return this.carts.push(pdt); }
 
-    addProduct(pdt){
-        return this.carts.push(pdt);
-    }
-
-    listCart(){
-        return `Your cart: ${this.carts}`;
-    }
+  listCart() { return `Your cart: ${this.carts}`; }
 }
 
-let p = new Cart();
+const p = new Cart();
 p.addProduct('book');
 p.addProduct('soda');
 p.addProduct('cake');
